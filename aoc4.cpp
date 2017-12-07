16a5f7bc5b4bff55129a6823b7ef21bd0fcd0eb4
Magnus Drougge <magnus.drougge@purplescout.se>
2017-12-07 23:27:46 +0100

    day 4 part 2

--- aoc4.cpp
+++ aoc4.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <map>
 #include <set>
+#include <cstring>
 
 namespace aoc4 {
 
@@ -75,12 +76,53 @@ namespace aoc4 {
   }
   
   /*
+    --- Part Two ---
+    
+    For added security, yet another system policy has been put in place.
+    Now, a valid passphrase must contain no two words that are anagrams
+    of each other - that is, a passphrase is invalid if any word's
+    letters can be rearranged to form any other word in the passphrase.
+    
+    For example:
+    
+    abcde fghij is a valid passphrase.
+    abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+    a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+    iiii oiii ooii oooi oooo is valid.
+    oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+
+    Under this new system policy, how many passphrases are valid?
    */
+
+  int
+  cmp(const void* m1, const void* m2) {
+    return *((const char*) m1) - *((const char*) m2);
+  }
+  
+  
   void
   solvePart2(int argc, char** argv) {
-    std::ifstream is("aoc2-1.input");
+    std::ifstream ifs("aoc4.input");
+    std::vector<std::vector<std::string>> data = loadInput(ifs);
+    
+    int valid = 0;
+    std::vector<std::vector<std::string>>::iterator passwords = data.begin();
+    while (passwords != data.end()) {
+      std::set<std::string> tokens{};
+      for (std::string t : *passwords) {
+	char tmp[t.size()];
+	std::strcpy(tmp, t.c_str());
+	
+	std::qsort(tmp, t.size(), sizeof(char), cmp);
+	tokens.insert(std::string{tmp});
+      }
 
-
-    std::printf("04 P 2: TBD\n");
+      if (tokens.size() == (*passwords).size()) {
+	valid++;
+      }
+      passwords++;
+    }
+    
+    std::printf("04 P 2: %d\n", valid);
   }
 }
